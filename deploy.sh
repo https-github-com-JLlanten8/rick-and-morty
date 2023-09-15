d8fb6450950cf4a562a05c52aeff9b2c73f3cfa1
Julian Llanten <hugojulians@gmail.com>
2023-09-15 16:07:38 -0500

    Update file deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+echo "Deploy..."
 
 # aborta en caso de errores
 set -e
@@ -15,6 +16,9 @@ git commit -m 'deploy'
 
 # si está usando https://<USERNAME>.github.io/<REPO>
 git push -f https://github.com/https-github-com-JLlanten8/rick-and-morty/public.git master:gh-pages
+
+#imprime el mensaje de finalización y dame el url de github pages
+echo "Deploy completado en https://https-github-com-jllanten8.github.io/rick-and-morty/public/"
 
 # si está usando https://<USERNAME>.github.io/<REPO>
 cd -
